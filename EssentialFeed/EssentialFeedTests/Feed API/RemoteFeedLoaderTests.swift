382725d111b04265868be2854f3dcdf70657b494
amitagroya <amitksoni.it@gmail.com>
2023-04-25 07:39:57 +0530

    Move test file to group to denote its module (Feed API)

--- EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -93,9 +93,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         var sut: RemoteFeedLoader? = RemoteFeedLoader(url: url, client: client)
         
         var capturedResults = [RemoteFeedLoader.Result]()
-        sut?.load {
-            capturedResults.append($0)
-        }
+        sut?.load { capturedResults.append($0) }
         sut = nil
         client.complete(withStatusCode: 200, data: makeItemsJSON([]))
         
